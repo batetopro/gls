7664b2b2cd0778155b367b71cf0bafb73556db3c
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2017-09-20 15:37:13 +0200

    add current main.cpp with option to build as upper bound estimator

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-
-#include "graph.h"
-#include "graph_io.h"
-#include "gls.h"
+#include "../data_structure/graph.h"
+#include "../data_structure/io/graph_io.h"
+#include "../coloring/gls.h"
 
 int main(int argc, const char* argv[]) {
     srand(0);
@@ -19,10 +18,16 @@ int main(int argc, const char* argv[]) {
 	graph_access G;
 	graph_io::readGraphWeighted(G, graph_filename);
 	
+	#ifdef UPPER
+	std::cout << gls::ColoringUpperBound::simple(G) << ",";
+	std::cout << gls::ColoringUpperBound::theorem2(G) << ",";
+	std::cout << gls::ColoringUpperBound::theorem3(G) << ",";
+	std::cout << gls::EpocheRunner::get_colors(gls::ColoringBuilder::greedy(G)) << std::endl;
+	#else
 	gls::init();
 	gls::ColoringBuilder builder = gls::ColoringBuilder();
 	gls::EpocheRunner runner = gls::EpocheRunner();
 	gls::colors result = runner.solve(G, builder.build(G));
-	
+	#endif
 	return 0;
  }
